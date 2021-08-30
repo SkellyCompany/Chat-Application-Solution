0ce3b00e749ff3c5cc9ea70ce4113122b9ccac38
schemabuoi <charyszczakg@gmail.com>
2021-08-30 10:50:45 +0200

    Add .gitignore & scripts

---
+++ scripts/setup-project.sh
@@ -0,0 +1,13 @@
+cd ../ChatApplication
+
+### Pull Web App
+git clone git@github.com:Skelly-Co/Chat-Application-Web.git Web
+cd Web
+git checkout development
+cd ..
+
+### Pull API
+git clone git@github.com:Skelly-Co/Chat-Application-API.git API
+cd API
+git checkout development
+cd ..
